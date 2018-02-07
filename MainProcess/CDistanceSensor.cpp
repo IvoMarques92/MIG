f8f00d57350491c68d59fa0c7ad5ca898ab6bc3c
IvoMarques92 <ivomarques92@gmail.com>
2018-02-07 18:59:30 +0000

    ADD function to work and use the I2C with ADS1115. Read values from ADC.

--- MainProcess/CDistanceSensor.cpp
+++ MainProcess/CDistanceSensor.cpp
@@ -1,34 +1,41 @@
 #include "CDistanceSensor.h"
 
 CDistanceSensor::CDistanceSensor() {
-
+    device = "/dev/i2c-1";
+    addrI2C = 0x48;
+    distance = 0;
+    voltsPerBit = 3.3 / 32768.0;
 }
 
 CDistanceSensor::~CDistanceSensor()
 {
 
 }
+void CDistanceSensor::startReadCDistanceSensor()
+{
+    uint8_t writeBuffer[] = {1,                     //config register -> from pag.28 Datasheet ADS1115
+                             0b11000011,            //Single Conversion, AIN0, 4.096V, Single Shoot mode
+                             0b11100111};           //860SPS, Disable comparator
 
-/*******************************************************************************
-* Function Name  : initCDistanceSensor
-* Description    : Initialize distance sensor
-* Input          : None (void)
-* Output         : None (void)
-* Return		 : None
-*******************************************************************************/
-void CDistanceSensor::initCDistanceSensor() {
-    return;
-}
+    if ((fd = open("/dev/i2c-1", O_RDWR)) < 0) {
+        printf("ERROR:Can not open device!");
+    }
 
-/*******************************************************************************
-* Function Name  : closeCDistanceSensor
-* Description    : Close distance sensor
-* Input          : None (void)
-* Output         : None (void)
-* Return		 : None
-*******************************************************************************/
-void CDistanceSensor::closeCDistanceSensor() {
-    return;
+    if (ioctl(fd, I2C_SLAVE, addrI2C) < 0) {
+        printf("ERROR: Can not find the address!");
+    }
+
+    if (write(fd, writeBuffer, 3) != 3) {
+        printf("ERROR: Can not write on device");
+    }
+    writeBuffer[0] = 0;
+    if (write(fd,writeBuffer, 1) != 1) {
+        perror("Write register select");
+        exit(-1);
+    }
+
+    close(fd);
+
 }
 
 /*******************************************************************************
@@ -38,8 +45,27 @@ void CDistanceSensor::closeCDistanceSensor() {
 * Output         : None (void)
 * Return		 : None
 *******************************************************************************/
-uint16_t CDistanceSensor::readCDistanceSensor() {
-    return 0;
+float CDistanceSensor::readCDistanceSensor() {
+    readBuffer[0] = 0;
+    readBuffer[1] = 0;
+
+    if ((fd = open("/dev/i2c-1", O_RDONLY)) < 0) {
+        printf("ERROR:Can not open device!");
+    }
+
+    if (ioctl(fd, I2C_SLAVE, addrI2C) < 0) {
+        printf("ERROR: Can not find the address!");
+    }
+
+    if (read(fd, readBuffer, 2) != 2) {
+        printf("ERROR: Can not read on device");
+    }
+
+    close(fd);
+
+    distance = voltsPerBit * (readBuffer[0]<<8 | readBuffer[1]);
+
+    return distance;
 }
 
 CDistanceSensor* CDistanceSensor::instance = 0;
